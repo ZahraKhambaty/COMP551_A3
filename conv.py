83d35b744b1f2b26316ee36230197d0720e46c10
Robert Fratila <fratilarobert@hotmail.com>
2017-03-12 12:56:17 -0400

    Updated training script for random selection and wrote up logistic regression

--- conv.py
+++ conv.py
@@ -3,7 +3,7 @@ import scipy.io as sio
 import theano
 import lasagne
 import theano.tensor as T
-import os
+import os, sys
 
 from collections import OrderedDict
 import pylab #for graphing
@@ -11,6 +11,8 @@ import pylab #for graphing
 from random import shuffle
 
 import time
+import math
+from sklearn.model_selection import train_test_split
 
 def createNetwork(dimensions, input_var):
 	#dimensions = (1,1,data.shape[0],data.shape[1]) #We have to specify the input size because of the dense layer
@@ -23,29 +25,27 @@ def createNetwork(dimensions, input_var):
 	print '	',lasagne.layers.get_output_shape(network)
 	print ('Hidden Layer:')
 
-	network = lasagne.layers.Conv2DLayer(network, num_filters=64, filter_size=(5,5), pad ='same',nonlinearity=lasagne.nonlinearities.rectify)
-	network = lasagne.layers.Conv2DLayer(network, num_filters=64, filter_size=(5,5), pad ='same',nonlinearity=lasagne.nonlinearities.rectify)
-
+	#network = lasagne.layers.Conv2DLayer(network, num_filters=64, filter_size=(5,5), pad ='same',nonlinearity=lasagne.nonlinearities.rectify)
+	network = lasagne.layers.Conv2DLayer(network, num_filters=128, filter_size=(5,5), pad ='same',nonlinearity=lasagne.nonlinearities.rectify)
 	network = lasagne.layers.MaxPool2DLayer(network,pool_size=(2, 2))
 	print '	',lasagne.layers.get_output_shape(network)
-
-	network = lasagne.layers.Conv2DLayer(network, num_filters=128, filter_size=(3,3), pad='same',nonlinearity=lasagne.nonlinearities.rectify)
-	network = lasagne.layers.Conv2DLayer(network, num_filters=128, filter_size=(3,3), pad='same',nonlinearity=lasagne.nonlinearities.rectify)
 	
-	network = lasagne.layers.MaxPool2DLayer(network,pool_size=(2, 2))
-	print '	',lasagne.layers.get_output_shape(network)
-
-	network = lasagne.layers.Conv2DLayer(network, num_filters=256, filter_size=(3,3), pad='same',nonlinearity=lasagne.nonlinearities.rectify)
+	#network = lasagne.layers.Conv2DLayer(network, num_filters=128, filter_size=(3,3), pad='same',nonlinearity=lasagne.nonlinearities.rectify)
 	network = lasagne.layers.Conv2DLayer(network, num_filters=256, filter_size=(3,3), pad='same',nonlinearity=lasagne.nonlinearities.rectify)
 	network = lasagne.layers.MaxPool2DLayer(network,pool_size=(2, 2))
 	print '	',lasagne.layers.get_output_shape(network)
-
+	
+	#network = lasagne.layers.Conv2DLayer(network, num_filters=256, filter_size=(3,3), pad='same',nonlinearity=lasagne.nonlinearities.rectify)
+	network = lasagne.layers.Conv2DLayer(network, num_filters=256, filter_size=(3,3), pad='same',nonlinearity=lasagne.nonlinearities.rectify)
+	network = lasagne.layers.MaxPool2DLayer(network,pool_size=(2, 2))
+	print '	',lasagne.layers.get_output_shape(network)
+	'''
 	network = lasagne.layers.DenseLayer(network,num_units=2048,nonlinearity=lasagne.nonlinearities.rectify)
 	print '	',lasagne.layers.get_output_shape(network)
 
 	network = lasagne.layers.DenseLayer(network,num_units=1024,nonlinearity=lasagne.nonlinearities.rectify)
 	print '	',lasagne.layers.get_output_shape(network)
-
+	'''
 	network = lasagne.layers.DenseLayer(network, num_units=40, nonlinearity = lasagne.nonlinearities.softmax)
 	print ('Output Layer:')
 	print '	',lasagne.layers.get_output_shape(network)
@@ -92,38 +92,29 @@ def loadModel(network, model='brain1.npz'):
 
 
 def main():
-	dataPath = 'data'
-	testReserve = 0.1
-	validationReserve = 0.1
-	trainingReserve = 1-(testReserve+validationReserve)
+
+	#testReserve = 0.1
+	#validationReserve = 0.2
+	#trainingReserve = 1-(testReserve+validationReserve)
 	input_var = T.tensor4('input')
 	y = T.dmatrix('truth')
 
 	trainFromScratch = True
-	epochs = 10
-	samplesperEpoch = 10
+	#epochs = 10
+	samplesperEpoch = 100 # how many divisions there are in the data when training since it all can't fit in memory
 
 	#MODIFY THESE
 	#*******************************************
-	trainTime = 10.0/60 #in hours
-	modelName='whatever_you_want'
+	trainTime = 1 #in hours
+	modelName='A3'
 	#*******************************************
 
-
-	#for image in [dataPath]:
-
-	#	imagePath = os.path.join(dataPath,'attentive')
-	#	labelPath = os.path.join(dataPath,'inattentive')
-
-		#if os.path.exists(attentivePath) and os.path.exists(inattentivePath):
-		#	trainX += [os.path.join(attentivePath,i) for i in os.listdir(attentivePath)]
-		#	trainX += [os.path.join(inattentivePath,i) for i in os.listdir(inattentivePath)  if i.endswith('.json')]
-		#	shuffle(trainX)
 
 	trainX = numpy.load('data/tinyX.npy') # this should have shape (26344, 3, 64, 64)
 	trainY = numpy.load('data/tinyY.npy') 
 	print ("%i samples found"%trainX.shape[0])
 	#Set up truth values
+
 	new_y = numpy.array([numpy.zeros(40)],dtype='float32')
 	for categ in trainY:
 		temp = numpy.array([numpy.zeros(40)],dtype='float32')
@@ -131,76 +122,70 @@ def main():
 		new_y = numpy.concatenate((new_y,temp),axis=0)
 	new_y = new_y[1:]
 	
-	
 	#This reserves the correct amount of samples for training, testing and validating
-	trainingSet = trainX[:int(trainingReserve*trainX.shape[0])]
-	testSet = trainX[int(trainingReserve*trainX.shape[0]):-int(testReserve*trainX.shape[0])]
-	validationSet = trainX[int(testReserve*trainX.shape[0] + int(trainingReserve*trainX.shape[0])):]
+	#trainingSet = trainX[:int(trainingReserve*trainX.shape[0])]
+	#testSet = trainX[int(trainingReserve*trainX.shape[0]):-int(testReserve*trainX.shape[0])]
+	#validationSet = trainX[int(testReserve*trainX.shape[0] + int(trainingReserve*trainX.shape[0])):]
 
-	trainingLabel = new_y[:int(trainingReserve*new_y.shape[0])]
-	testLabel = new_y[int(trainingReserve*new_y.shape[0]):-int(testReserve*new_y.shape[0])]
-	validationLabel = new_y[int(testReserve*new_y.shape[0] + int(trainingReserve*new_y.shape[0])):]
+	#trainingLabel = new_y[:int(trainingReserve*new_y.shape[0])]
+	#testLabel = new_y[int(trainingReserve*new_y.shape[0]):-int(testReserve*new_y.shape[0])]
+	#validationLabel = new_y[int(testReserve*new_y.shape[0] + int(trainingReserve*new_y.shape[0])):]
 	
+	trainingSet, validationSet, trainingLabel, validationLabel = train_test_split(trainX,new_y, test_size=0.3, random_state=42)
 
-
-	
 	networkDimensions = (None,trainingSet.shape[1],trainingSet.shape[2],trainingSet.shape[3])
 	network  = createNetwork(networkDimensions, input_var)
 	trainer = createTrainer(network,input_var,y)
-
 	validator = createValidator(network,input_var,y)
 
 	if not trainFromScratch:
 		print ('loading a previously trained model...\n')
-		network = loadModel(network,'Emily2Layer300000.npz')
+		network = loadModel(network,'A3.npz')
 
 
 	#print ("Training for %s epochs with %s samples per epoch"%(epochs,samplesperEpoch))
-	record = OrderedDict(epoch=[],error=[],accuracy=[])
-
+	record = OrderedDict(epoch=[],tr_error=[],tr_accuracy=[],v_error=[],v_accuracy=[])
 	print ("Training for %s hour(s) with %s samples per epoch"%(trainTime,samplesperEpoch))
 	epoch = 0
-	sample_range = 100
 	startTime = time.time()
 	timeElapsed = time.time() - startTime
+
 	#for epoch in xrange(epochs):            #use for epoch training
-	while timeElapsed/3600 < trainTime :     #use for time training
+	while (time.time()-startTime)/3600 < trainTime :     #use for time training
 		epochTime = time.time()
 		print ("--> Epoch: %d | Time left: %.2f hour(s)"%(epoch,trainTime-timeElapsed/3600))
+		chunk = int(math.ceil(trainingSet.shape[0]/samplesperEpoch))
 		for i in xrange(samplesperEpoch):
 
-			chooseRandomly = numpy.random.randint(trainingSet.shape[0] - sample_range)
-			data = trainingSet[chooseRandomly:chooseRandomly+sample_range]
-			label = trainingLabel[chooseRandomly:chooseRandomly+sample_range]
-			#trainIn = data.reshape(list(data.shape))
-			import pudb; pu.db
+			data = trainingSet[chunk*i:chunk*i + chunk]
+			label = trainingLabel[chunk*i:chunk*i + chunk]
 			trainer(data, label)
+			timeElapsed = time.time() - epochTime
+			percentage = float(i+1) / float(samplesperEpoch) * 100
+			sys.stdout.flush()
+			sys.stdout.write("\r %d training steps complete: %.2f%% done epoch in %ds" % (i + 1, percentage, timeElapsed))
 
-		chooseRandomly = numpy.random.randint(trainingSet.shape[0] - sample_range)
-		#print ("Gathering data...%s"%validationSet[chooseRandomly])
-		val_data = trainingSet[chooseRandomly:chooseRandomly+sample_range]
-		val_label = trainingLabel[chooseRandomly:chooseRandomly+sample_range]
-		#trainIn = val_data.reshape(list(val_data.shape))
-
-		error, accuracy = validator(val_data, val_label)			     #pass modified data through network
-		record['error'].append(error)
-		record['accuracy'].append(accuracy)
+		tr_error, tr_accuracy = validator(trainingSet[:4000], trainingLabel[:4000])
+		v_error, v_accuracy = validator(validationSet[:4000], validationLabel[:4000])			     #pass modified data through network
+		record['tr_error'].append(tr_error); record['tr_accuracy'].append(tr_accuracy)
+		record['v_error'].append(v_error); record['v_accuracy'].append(v_accuracy)
 		record['epoch'].append(epoch)
-		timeElapsed = time.time() - startTime
 		epochTime = time.time() - epochTime
-		print ("	error: %s and accuracy: %s in %.2fs\n"%(error,accuracy,epochTime))
+		print ("\n	error: %s and accuracy: %s in %.2fs\n"%(v_error,v_accuracy,epochTime))
 		epoch+=1
 
-	validateNetwork(network,input_var,testSet)
+	#validateNetwork(network,input_var,testSet)
 
 	saveModel(network=network,modelName=modelName)
 	#import pudb; pu.db
 	#save metrics to pickle file to be opened later and displayed
 	import pickle
-	#data = {'data':record}
+
 	with open('%sstats.pickle'%modelName,'w') as output:
-		#import pudb; pu.db
+
 		pickle.dump(record,output)
+
+	import pudb; pu.db
 	
 if __name__ == "__main__":
     main()
